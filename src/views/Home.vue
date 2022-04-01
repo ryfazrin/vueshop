196df6d402f5a80626bd835bcbaeeb06b545f76c
ryfazrin <riansera515@gmail.com>
2022-04-01 16:27:43 +0800

    added Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,100 @@
 <template>
-  <hello-world />
+  <div>
+    <!-- template categories -->
+    <v-conteiner class="ma-0 pa-0" grid-list-sm>
+      <div class="text-right">
+        <v-btn small text to="/categories" class="blue--text">
+          All Categories <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </div>
+      <v-layout wrap>
+        <v-flex
+          v-for="category in categories"
+          :key="`category-` + category.id"
+          class="pa-1"
+          xs6
+        >
+          <v-card :to="'/category/' + category.slug">
+            <v-img :src="category.image" class="white--text">
+              <v-card-title
+                class="fill-height align-end"
+                v-text="category.name"
+              ></v-card-title>
+            </v-img>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-conteiner>
+
+    <!-- template books -->
+    <v-container class="ma-0 pa-0 mt-2">
+      <div class="text-right">
+        <v-btn small text to="/books" class="blue--text">
+          All Books <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </div>
+      <v-layout wrap>
+        <v-flex v-for="book in books" :key="`book-` + book.id" class="pa-1" xs6>
+          <v-card :to="'/book/' + book.slug">
+            <v-img :src="book.cover" class="white--text">
+              <v-card-title
+                class="fill-right align-end"
+                v-text="book.title"
+              ></v-card-title>
+            </v-img>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
   name: "Home",
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      categories: [
+        {
+          id: 1,
+          image: "https://via.placeholder.com/150",
+          name: "Ekonomi",
+          slug: "ekonomi",
+        },
+        {
+          id: 2,
+          image: "https://via.placeholder.com/150",
+          name: "Agama",
+          slug: "agama",
+        },
+      ],
+      books: [
+        {
+          id: 1,
+          cover: "https://via.placeholder.com/150",
+          title: "Laravel 6.0",
+          slug: "laravel-6-0",
+        },
+        {
+          id: 2,
+          cover: "https://via.placeholder.com/150",
+          title: "Vue 2.6",
+          slug: "vue-2-6",
+        },
+        {
+          id: 3,
+          cover: "https://via.placeholder.com/150",
+          title: "PHP 7.4",
+          slug: "php-7-4",
+        },
+        {
+          id: 4,
+          cover: "https://via.placeholder.com/150",
+          title: "NodeJS 14",
+          slug: "nodejs-14",
+        },
+      ],
+    };
   },
 };
 </script>
