7d77723913df5734ae35e8be10a0f3401c48ad53
ryfazrin <riansera515@gmail.com>
2022-04-03 12:26:33 +0800

    added dynamic header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark extended>
+    <v-app-bar app color="primary" dark v-if="isHome">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>{{ appName }}</v-toolbar-title>
 
@@ -24,6 +24,23 @@
         prepend-inner-icon="mdi-magnify"
         solo-inverted
       ></v-text-field>
+    </v-app-bar>
+
+    <v-app-bar app color="primary" dark v-else>
+      <v-btn icon @click.stop="$router.go(-1)">
+        <v-icon>mdi-arrow-left-circle</v-icon>
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon to="/about">
+        <v-badge color="orange" overlap>
+          <template v-slot:badge>
+            <span>3</span>
+          </template>
+          <v-icon>mdi-cart</v-icon>
+        </v-badge>
+      </v-btn>
     </v-app-bar>
 
     <v-card>
@@ -100,7 +117,6 @@
 <script>
 export default {
   name: "App",
-
   data: () => ({
     drawer: false,
     menus: [
@@ -109,5 +125,10 @@ export default {
     ],
     guest: true,
   }),
+  computed: {
+    isHome() {
+      return (this.$route.path == '/')
+    }
+  }
 };
 </script>
