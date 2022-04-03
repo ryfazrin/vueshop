06a8ee6ca8ea12d021df17e389efc1da4e350d74
ryfazrin <riansera515@gmail.com>
2022-04-03 11:57:47 +0800

    create Categories page

---
+++ src/views/Categories.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-container class="ma-0 pa-0" grid-list-sm>
+      <v-subheader>
+        <v-layout wrap>
+          <v-flex
+            v-for="category in categories"
+            :key="`category-` + category.id"
+            xs6
+          >
+            <v-card :to="'/category/' + category.slug">
+              <v-img :src="getImage('/categories/' + category.image)">
+                <v-card-title
+                  class="fill-height align-end"
+                  v-text="category.name"
+                ></v-card-title>
+              </v-img>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-subheader>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      categories: [],
+    }
+  },
+  created() {
+
+  },
+}
+</script>
+
+<style>
+</style>
