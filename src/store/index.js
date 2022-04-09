5fa678c3114978ffa34fc0b6fa0baea185e39887
ryfazrin <riansera515@gmail.com>
2022-04-09 14:23:37 +0800

    define mutations carts

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,33 @@ export default new Vuex.Store({
     carts: [],
   },
   mutations: {
+    insert: (state, payload) => {
+      state.carts.push({
+        id: payload.id,
+        title: payload.title,
+        cover: payload.cover,
+        price: payload.price,
+        weight: payload.weight,
+        quantity: 1
+      })
+    },
+    update: (state, payload) => {
+      // mendeteksi payload ada di index keberapa?
+      let idx = state.carts.indexOf(payload)
+      state.carts.splice(idx, 1, {
+        id: payload.id,
+        title: payload.title,
+        cover: payload.cover,
+        price: payload.price,
+        weight: payload.weight,
+        quantity: payload.quantity
+      })
+
+      if (payload.quantity <= 0) {
+        // menghapus item carts jika quantity-nya nol
+        state.carts.splice(idx, 1)
+      }
+    }
   },
   actions: {
   },
