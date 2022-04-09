31df2a34a635d57a83de25628168c1d0311f3ba1
ryfazrin <riansera515@gmail.com>
2022-04-09 12:48:18 +0800

    create BookItem component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-card :to="'/book/' + book.slug">
+      <v-img
+        :src="getImage('/books/' + book.cover)"
+        class="white--text"
+      >
+        <v-card-title
+          class="fill-height align-end"
+          v-text="book.title"
+        ></v-card-title>
+      </v-img>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'book-item',
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
