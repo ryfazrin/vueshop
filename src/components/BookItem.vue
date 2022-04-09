c484e23429d6be5765fda6a010736127977e800b
ryfazrin <riansera515@gmail.com>
2022-04-09 12:50:57 +0800

    added BookItem template

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -4,12 +4,22 @@
       <v-img
         :src="getImage('/books/' + book.cover)"
         class="white--text"
+        height="200px"
       >
         <v-card-title
           class="fill-height align-end"
           v-text="book.title"
         ></v-card-title>
       </v-img>
+
+      <v-card-actions>
+        <v-icon>mdi-cash</v-icon>
+        <span class="orange--text"
+          >Rp {{ book.price.toLocaleString("id-ID") }}
+        </span>
+        <v-spacer></v-spacer>
+        <v-icon>mdi-eye</v-icon> {{ book.views }}
+      </v-card-actions>
     </v-card>
   </div>
 </template>
@@ -17,14 +27,14 @@
 <script>
 export default {
   name: 'book-item',
+  props: ['book'],
   data() {
     return {
-      
+
     }
   },
 }
 </script>
 
 <style>
-
 </style>
